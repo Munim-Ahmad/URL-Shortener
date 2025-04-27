0f98fe7ed0ad66141c35cec0d39682af0c011fde
Munim-Ahmad <munimahmad2@gmail.com>
2025-04-27 16:31:28 +0500

    Set up mongoDB and finalized project

--- server.js
+++ server.js
@@ -1,18 +1,41 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const ShortUrl = require('./models/shortUrl.js');
 const app = express();
 
-mongoose
-  .connect('mongodb://localhost/urlShortener', {})
-  .then(() => console.log('MongoDb connected'))
-  .catch((err) => console.log('MongoDB connection error', err));
+async function startServer() {
+  try {
+    await mongoose.connect('mongodb://localhost/urlShortener');
 
-app.set('view engine', 'ejs');
+    app.set('view engine', 'ejs');
+    app.use(express.urlencoded({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.render('index');
-});
+    app.get('/', async (req, res) => {
+      const shortUrls = await ShortUrl.find();
+      res.render('index', { shortUrls: shortUrls });
+    });
 
-app.post('/shortUrls', (req, res) => {});
+    app.post('/shortUrls', async (req, res) => {
+      await ShortUrl.create({ full: req.body.fullUrl });
 
-app.listen(process.env.PORT || 3000);
+      res.redirect('/');
+    });
+
+    app.get('/:shortUrl', async (req, res) => {
+      const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+      if (shortUrl == null) return res.sendStatus(404);
+
+      shortUrl.clicks++;
+      shortUrl.save();
+
+      res.redirect(shortUrl.full);
+    });
+
+    app.listen(process.env.PORT || 3000);
+  } catch (err) {
+    console.log('MongoDB connection eroor', err);
+    process.exit(1);
+  }
+}
+
+startServer();
